de1bbb360fa39852f1675ab5017749d2c865668c
huangzhuangzhuang <huangzhuangzhuang@wangdian.cn>
2020-02-20 16:12:03 +0800

    lombok简化User

--- src/org/hzz/excel/pojo/User.java
+++ src/org/hzz/excel/pojo/User.java
@@ -1,37 +1,12 @@
 package org.hzz.excel.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
     private Integer Id;
     private String name;
     private String phone;
-
-    public User(Integer id, String name, String phone) {
-        Id = id;
-        this.name = name;
-        this.phone = phone;
-    }
-
-    public Integer getId() {
-        return Id;
-    }
-
-    public void setId(Integer id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
